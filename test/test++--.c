223e94c4c66a881db304099f8f27265fb5f84ba1
Anaconda-cjp <86179019+Anaconda-cjp@users.noreply.github.com>
2023-05-31 10:46:20 +0800

    补交用于自增自减测试的c代码

---
+++ test/test++--.c
@@ -0,0 +1,18 @@
+void main()
+{
+    Int m;Int n;
+    m = 1;
+    n = m++;
+    print m;
+    print n;
+    n = m--;
+    print m;
+    print n;
+    n = --m;
+    print m;
+    print n;
+    n = ++m;
+    print m;   
+    print n; 
+
+}
