954287001a445f53c33995fa87a1f40c12c3f33b
wsy12312 <wsy17856950711@163.com>
2023-06-12 13:50:32 +0800

    编译器+=，dowhile，dountil等，完善报告

--- Comp.fs
+++ Comp.fs
@@ -186,6 +186,27 @@ let rec cStmt stmt (varEnv: VarEnv) (funEnv: FunEnv) : instr list =
         @ cStmt body varEnv funEnv
           @ [ Label labtest ]
             @ cExpr e varEnv funEnv @ [ IFNZRO labbegin ]
+
+    | DoWhile(body, e) -> 
+        let labbegin = newLabel ()
+        let labtest = newLabel ()
+        // 先执行一次DO 再进入循环
+        cStmt body varEnv funEnv
+        @[ GOTO labtest; Label labbegin ]
+        @ cStmt body varEnv funEnv
+          @ [ Label labtest ]
+            @ cExpr e varEnv funEnv @ [ IFNZRO labbegin ]
+
+    | DoUntil(body, e) -> 
+        let labbegin = newLabel ()
+        let labtest = newLabel ()
+        // 先执行一次DO 再进入循环
+        cStmt body varEnv funEnv
+        @[ GOTO labtest; Label labbegin ]
+        @ cStmt body varEnv funEnv
+          @ [ Label labtest ]
+            @ cExpr e varEnv funEnv @ [ IFZERO labbegin ]
+            
     | Expr e -> cExpr e varEnv funEnv @ [ INCSP -1 ]
     | Block stmts ->
 
